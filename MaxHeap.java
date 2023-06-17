e8b96ac4cfc9296f87f770a56905166e87645f1f
BastleyNait <schirinosne@unsa.edu.pe>
2023-06-17 09:15:33 -0500

    modificaciones

--- MaxHeap.java
+++ MaxHeap.java
@@ -1,8 +1,8 @@
-package heapArr;
 
 import java.util.ArrayList;
 
 public class MaxHeap<T extends Comparable<T>> {
+    // ArrayList para almacenar los elementos del heap
     private ArrayList<T> heapArr;
 
     public MaxHeap() {
@@ -13,48 +13,54 @@ public class MaxHeap<T extends Comparable<T>> {
         return heapArr.isEmpty();
     }
 
+    // Inserta un elemento en el heap y lo eleva a su posición adecuada
+    public void insert(T item) {
+        heapArr.add(item);
+        elevar(heapArr.size() - 1);
+    }
+
+    // Eleva un elemento en el índice dado hasta su posición adecuada en el heap
+    private void elevar(int childIndex) {
+        int parentIndex = (childIndex - 1) / 2;
+        T childItem = heapArr.get(childIndex);
+
+        while (childIndex > 0 && childItem.compareTo(heapArr.get(parentIndex)) > 0) {
+            heapArr.set(childIndex, heapArr.get(parentIndex));
+            childIndex = parentIndex;
+            parentIndex = (childIndex - 1) / 2;
+        }
+
+        heapArr.set(childIndex, childItem);
+    }
+
+    // Hunde un elemento en el índice dado hasta su posición adecuada en el heap
+    private void undir(int parentIndex) {
+        int leftChildIndex = 2 * parentIndex + 1;
+        int rightChildIndex = 2 * parentIndex + 2;
+        int largestIndex = parentIndex;
+
+        if (leftChildIndex < heapArr.size() && heapArr.get(leftChildIndex).compareTo(heapArr.get(largestIndex)) > 0) {
+            largestIndex = leftChildIndex;
+        }
+
+        if (rightChildIndex < heapArr.size() && heapArr.get(rightChildIndex).compareTo(heapArr.get(largestIndex)) > 0) {
+            largestIndex = rightChildIndex;
+        }
+
+        if (largestIndex != parentIndex) {
+            swap(parentIndex, largestIndex);
+            undir(largestIndex);
+        }
+    }
+
+    // Intercambia dos elementos en los índices dados
     private void swap(int index1, int index2) {
         T temp = heapArr.get(index1);
         heapArr.set(index1, heapArr.get(index2));
         heapArr.set(index2, temp);
     }
 
-    public void elevar(T item) {
-        heapArr.add(item);
-        elevar(heapArr.size() - 1);
-    }
-
-    private void elevar(int childI) {
-        int parentI = (childI - 1) / 2;
-        T childItem = heapArr.get(childI);
-
-        while (childI > 0 && childItem.compareTo(heapArr.get(parentI)) > 0) {
-            heapArr.set(childI, heapArr.get(parentI));
-            childI = parentI;
-            parentI = (childI - 1) / 2;
-        }
-        heapArr.set(childI, childItem);
-    }
-
-    private void undir(int parentI) {
-        int lChildI = 2 * parentI + 1;
-        int rChildI = 2 * parentI + 2;
-        int largestI = parentI;
-
-        if (lChildI < heapArr.size() && heapArr.get(lChildI).compareTo(heapArr.get(largestI)) > 0) {
-            largestI = lChildI;
-        }
-
-        if (rChildI < heapArr.size() && heapArr.get(rChildI).compareTo(heapArr.get(largestI)) > 0) {
-            largestI = rChildI;
-        }
-
-        if (largestI != parentI) {
-            swap(parentI, largestI);
-            undir(largestI);
-        }
-    }
-
+    // Elimina y devuelve el elemento de mayor valor (raíz del heap)
     public T removeMax() {
         if (isEmpty()) {
             throw new IllegalStateException("Heap is empty");
--- testHeap.java
+++ testHeap.java
@@ -1,2 +1,15 @@
-package heapArr;public class testHeap {
+
+public class testHeap {
+    public static void main(String[] args) {
+        MaxHeap<Integer> heap = new MaxHeap<>();
+        heap.insert(2);
+        heap.insert(5);
+        heap.insert(6);
+        heap.insert(8);
+        heap.insert(3);
+        heap.insert(10);
+        heap.insert(1);
+        heap.insert(7);
+        System.out.println(heap);
+    }
 }
